baa15467f1511f158db5d4005b6ff6a81f0c651a
MrLeeh <Stefan.St.Lehmann@gmail.com>
2013-09-29 21:01:50 +0200

    changed some package infos

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
       name = "adsPy",
       version = "1.0.2",
-      description = "Python wrapper for Beckhoff ADS-DLL",
+      description = "Python wrapper for TwinCAT ADS-DLL",
       author = "Stefan Lehmann",
       author_email = "mrleeh@gmx.de",
       packages = ["adsPy"],
       package_data = {'adsPy': ['doc/*.*']},
       requires = ['ctypes'],
       provides=['adsPy'],
-      url = 'http://mrleeh.square7.ch/'
+      url = 'https://github.com/MrLeeh/adsPy'
       )
