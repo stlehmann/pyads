3177571b560ee61e9660c74a5935eb4001152f85
MrLeeh <Stefan.St.Lehmann@gmail.com>
2015-07-15 08:00:36 +0200

    include meta info in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
       name = "pyads",
-      version = "1.1.0",
+      version = "1.1.1",
       description = "Python wrapper for TwinCAT ADS library",
       author = "Stefan Lehmann",
       author_email = "Stefan.St.Lehmann@gmail.com",
@@ -12,5 +12,12 @@ setup(
       package_data = {'pyads': ['doc/*.*']},
       requires = ['ctypes'],
       provides=['pyads'],
-      url = 'https://github.com/MrLeeh/pyads'
+      url = 'https://github.com/MrLeeh/pyads',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries'
+      ]
 )
