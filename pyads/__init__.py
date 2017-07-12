42c776e1155c0c2cf99a8caa35eefa0b057069c6
Peter Janeck <p.janeck@web.de>
2017-07-12 16:58:43 +0200

    added additional exports for the module

--- pyads/__init__.py
+++ pyads/__init__.py
@@ -1,10 +1,11 @@
 #!-*- coding: utf-8 -*-
 # from .pyads import *
-from .structs import AmsAddr
+from .structs import AmsAddr, NotificationAttrib
 
 from .ads import open_port, close_port, get_local_address, read_state, \
     write_control, read_device_info, write, read_write, read, \
-    read_by_name, write_by_name, add_route, delete_route
+    read_by_name, write_by_name, add_route, delete_route, add_device_notification, \
+    del_device_notification
 
 from .constants import PLCTYPE_BOOL, PLCTYPE_BYTE, PLCTYPE_DATE, PLCTYPE_DINT, \
     PLCTYPE_DT, PLCTYPE_DWORD, PLCTYPE_INT, PLCTYPE_LREAL, PLCTYPE_REAL, \
@@ -21,6 +22,9 @@ from .constants import ADSSTATE_INVALID, ADSSTATE_IDLE, ADSSTATE_RESET, \
     ADSSTATE_SAVECFG, ADSSTATE_LOADCFG, ADSSTATE_POWERFAILURE, \
     ADSSTATE_POWERGOOD, ADSSTATE_ERROR, ADSSTATE_SHUTDOWN, ADSSTATE_SUSPEND, \
     ADSSTATE_RESUME, ADSSTATE_CONFIG, ADSSTATE_RECONFIG
+
+from .constants import ADSTRANS_NOTRANS, ADSTRANS_CLIENTCYCLE, ADSTRANS_CLIENT1REQ, \
+    ADSTRANS_SERVERCYCLE, ADSTRANS_SERVERONCHA
 
 from ._version import get_versions
 __version__ = get_versions()['version']
