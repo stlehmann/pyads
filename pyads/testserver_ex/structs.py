06dbbea3aeae4cbcb00a86ccb897ff77a6031317
Stefan Lehmann <Lehmann@kuz-leipzig.de>
2017-09-21 15:30:12 +0200

    add AmsHeader to testserver structs

--- pyads/testserver_ex/structs.py
+++ pyads/testserver_ex/structs.py
@@ -1,59 +1,67 @@
 import struct
-from collections import namedtuple
-
-
-_AmsTcpHeader = namedtuple('AmsTcpHeader', 'length')
-
-_AmsHeader = namedtuple(
-    'AmsHeader', (
-        'target_net_id', 'target_port', 'source_net_id', 'source_port',
-        'command_id', 'state_flags', 'length', 'error_code', 'invoke_id',
-        'data'
-    )
-)
+from ..structs import SAmsNetId
 
 
 class AmsTcpHeader:
     """ First layer of a ADS packet. """
 
-    def __init__(self, data=None, length=None):
-        if data is not None:
-            assert isinstance(data, (bytes, bytearray))
-            assert len(data) == 6
-
-        if data is None:
-            self.data = b'\x00' * 6
-        else:
-            self.data = data
-
-        if length is not None:
-            self.length = length
-
-    @property
-    def length(self):
-        return struct.unpack('<I', self.data[2:6])[0]
-
-    @length.setter
-    def length(self, value):
-        assert isinstance(value, int)
-        self.data = self.data[0:2] + struct.pack('<I', value)
-
-
-class AmsHeader(_AmsHeader):
-    """ Second layer of an ADS packet. """
-
-    def __init__(self, data=None):
-
-        if data is None:
-            self.data = b'\x00' * 
+    def __init__(self, length=0):
+        self.length = length
 
     @staticmethod
     def from_bytes(data):
-        ams_header = AmsHeader(
-            # Extract target/source net ID's and ports
-            data[0:6], data[6:8], data[8:14], data[14:16],
-            # Extract command ID, state flags, and data length
-            data[16:18], data[18:20], data[20:24],
-            # Extract error code, invoke ID, and data
-            data[24:28], data[28:32], data[32:]
+        assert isinstance(data, (bytes, bytearray))
+        assert len(data) == 6
+
+        return AmsTcpHeader(struct.unpack('<I', data[2:6])[0])
+
+    def to_bytes(self):
+        return b'\x00' * 2 + struct.pack('<I', self.length)
+
+
+class AmsHeader:
+    """ Second layer of an ADS packet. """
+
+    def __init__(self, target_net_id, target_port, source_net_id, source_port,
+                 command_id, state_flags, length, error_code, invoke_id,
+                 data):
+
+        self.target_net_id = target_net_id
+        self.target_port = target_port
+        self.source_net_id = source_net_id
+        self.source_port = source_port
+        self.command_id = command_id
+        self.state_flags = state_flags
+        self.length = length
+        self.error_code = error_code
+        self.invoke_id = invoke_id
+        self.data = data
+
+    @staticmethod
+    def from_bytes(data):
+        return AmsHeader(
+            target_net_id=SAmsNetId.from_buffer(bytearray(data[0:6])),
+            target_port=struct.unpack('<H', data[6:8])[0],
+            source_net_id=SAmsNetId.from_buffer(bytearray(data[8:14])),
+            source_port=struct.unpack('<H', data[14:16])[0],
+            command_id=struct.unpack('<H', data[16:18])[0],
+            state_flags=struct.unpack('<H', data[18:20])[0],
+            length=struct.unpack('<I', data[20:24])[0],
+            error_code=struct.unpack('<I', data[24:28])[0],
+            invoke_id=struct.unpack('<I', data[28:32])[0],
+            data=data[32:],
+        )
+
+    def to_bytes(self):
+        return (
+            bytearray(self.target_net_id) +
+            struct.pack('<H', self.target_port) +
+            bytearray(self.source_net_id) +
+            struct.pack('<H', self.source_port) +
+            struct.pack('<H', self.command_id) +
+            struct.pack('<H', self.state_flags) +
+            struct.pack('<I', self.length) +
+            struct.pack('<I', self.error_code) +
+            struct.pack('<I', self.invoke_id) +
+            self.data
         )
--- tests/test_testserver_ex_structs.py
+++ tests/test_testserver_ex_structs.py
@@ -1,10 +1,12 @@
 import unittest
 
+import pyads
 from pyads.testserver_ex import structs
+from pyads.structs import AmsAddr
 
 
-class TestserverExTestCase(unittest.TestCase):
-    
+class TestserverExStructsTestCase(unittest.TestCase):
+
     def setUp(self):
         pass
 
@@ -15,16 +17,49 @@ class TestserverExTestCase(unittest.TestCase):
 
         # test for empty data
         x = structs.AmsTcpHeader()
-        self.assertEqual(b'\x00' * 6, x.data)
+        self.assertEqual(b'\x00' * 6, x.to_bytes())
         self.assertEqual(0, x.length)
 
         # check for correct unpacking
-        x = structs.AmsTcpHeader(b'\x00\x00\x1f\x00\x00\x00')
+        x = structs.AmsTcpHeader.from_bytes(b'\x00\x00\x1f\x00\x00\x00')
         self.assertEqual(31, x.length)
 
         # check for correct packing
         x.length = 255
-        self.assertEqual(b'\x00\x00\xff\x00\x00\x00', x.data)
+        self.assertEqual(b'\x00\x00\xff\x00\x00\x00', x.to_bytes())
+
+    def test_amsheader(self):
+        source_addr = AmsAddr('127.0.0.1.1.1', 59558)
+        target_addr = AmsAddr('172.38.1.1.1.1', 59559)
+
+        header1 = structs.AmsHeader(
+            target_net_id=target_addr.netIdStruct(),
+            target_port=target_addr.port,
+            source_net_id=source_addr.netIdStruct(),
+            source_port=source_addr.port,
+            command_id=pyads.constants.ADSCOMMAND_READDEVICEINFO,
+            state_flags=pyads.constants.ADSSTATEFLAG_COMMAND,
+            length=6,
+            error_code=1,
+            invoke_id=2,
+            data=bytearray(b'\x01' * 6)
+        )
+
+        data_bytes = header1.to_bytes()
+        header2 = structs.AmsHeader.from_bytes(data_bytes)
+
+        self.assertEqual(bytearray(header1.target_net_id.b),
+                         bytearray(header2.target_net_id.b))
+        self.assertEqual(header1.target_port, header2.target_port)
+        self.assertEqual(bytearray(header1.source_net_id),
+                         bytearray(header2.source_net_id))
+        self.assertEqual(header1.source_port, header2.source_port)
+        self.assertEqual(header1.command_id, header2.command_id)
+        self.assertEqual(header1.state_flags, header2.state_flags)
+        self.assertEqual(header1.length, header2.length)
+        self.assertEqual(header1.error_code, header2.error_code)
+        self.assertEqual(header1.invoke_id, header2.invoke_id)
+        self.assertEqual(header1.data, header2.data)
 
 
 if __name__ == '__main__':
