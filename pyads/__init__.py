17d29f8b39893b1598e1748808efc17311e6c32b
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2017-10-01 13:43:18 +0200

    add ADSError in main namespace

--- pyads/__init__.py
+++ pyads/__init__.py
@@ -7,6 +7,8 @@ from .ads import open_port, close_port, get_local_address, read_state, \
     read_by_name, write_by_name, add_route, delete_route, \
     add_device_notification, del_device_notification, Connection
 
+from .pyads import ADSError
+
 from .constants import PLCTYPE_BOOL, PLCTYPE_BYTE, PLCTYPE_DATE, \
     PLCTYPE_DINT, PLCTYPE_DT, PLCTYPE_DWORD, PLCTYPE_INT, PLCTYPE_LREAL, \
     PLCTYPE_REAL, PLCTYPE_SINT, PLCTYPE_STRING, PLCTYPE_TIME, PLCTYPE_TOD, \
