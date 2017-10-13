a09f89e779d488a342f59582cb2c0bc4c6eccf1a
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2017-10-13 22:46:21 +0200

    increase coverage

--- tests/test_testserver_ex_structs.py
+++ tests/test_testserver_ex_structs.py
@@ -16,50 +16,85 @@ class TestserverExStructsTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # def test_amstcpheader(self):
+    def test_amstcpheader(self):
 
-    #     # test for empty data
-    #     x = structs.AmsTcpHeader()
-    #     self.assertEqual(b'\x00' * 6, x.to_bytes())
-    #     self.assertEqual(0, x.length)
+        # test for empty data
+        x = structs.AmsTcpHeader()
+        self.assertEqual(b'\x00' * 6, x.to_bytes())
+        self.assertEqual(0, x.length)
 
-    #     # check for correct unpacking
-    #     x = structs.AmsTcpHeader.from_bytes(b'\x00\x00\x1f\x00\x00\x00')
+        # check for correct unpacking
+        x = structs.AmsTcpHeader.from_bytes(b'\x00\x00\x1f\x00\x00\x00')
 
-    #     # check for correct packing
-    #     x.length = 255
-    #     self.assertEqual(b'\x00\x00\xff\x00\x00\x00', x.to_bytes())
+        # check for correct packing
+        x.length = 255
+        self.assertEqual(b'\x00\x00\xff\x00\x00\x00', x.to_bytes())
 
-    # def test_amsheader(self):
-    #     source_addr = AmsAddr('127.0.0.1.1.1', 59558)
-    #     target_addr = AmsAddr('172.38.1.1.1.1', 59559)
+    def test_amsheader(self):
+        source_addr = AmsAddr('127.0.0.1.1.1', 59558)
+        target_addr = AmsAddr('172.38.1.1.1.1', 59559)
 
-    #     header1 = structs.AmsHeader(
-    #         target_net_id=target_addr.netIdStruct(),
-    #         target_port=target_addr.port,
-    #         source_net_id=source_addr.netIdStruct(),
-    #         source_port=source_addr.port,
-    #         command_id=pyads.constants.ADSCOMMAND_READDEVICEINFO,
-    #         state_flags=pyads.constants.ADSSTATEFLAG_COMMAND,
-    #         length=6,
-    #         error_code=1,
-    #         invoke_id=2,
-    #     )
+        header1 = structs.AmsHeader(
+            target_net_id=target_addr.netIdStruct(),
+            target_port=target_addr.port,
+            source_net_id=source_addr.netIdStruct(),
+            source_port=source_addr.port,
+            command_id=pyads.constants.ADSCOMMAND_READDEVICEINFO,
+            state_flags=pyads.constants.ADSSTATEFLAG_COMMAND,
+            data_length=6,
+            error_code=1,
+            invoke_id=2,
+        )
 
-    #     data_bytes = header1.to_bytes()
-    #     header2 = structs.AmsHeader.from_bytes(data_bytes)
+        data_bytes = header1.to_bytes()
+        self.assertEqual(len(data_bytes), header1.length)
+        header2 = structs.AmsHeader.from_bytes(data_bytes)
 
-    #     self.assertEqual(bytearray(header1.target_net_id.b),
-    #                      bytearray(header2.target_net_id.b))
-    #     self.assertEqual(header1.target_port, header2.target_port)
-    #     self.assertEqual(bytearray(header1.source_net_id),
-    #                      bytearray(header2.source_net_id))
-    #     self.assertEqual(header1.source_port, header2.source_port)
-    #     self.assertEqual(header1.command_id, header2.command_id)
-    #     self.assertEqual(header1.state_flags, header2.state_flags)
-    #     self.assertEqual(header1.length, header2.length)
-    #     self.assertEqual(header1.error_code, header2.error_code)
-    #     self.assertEqual(header1.invoke_id, header2.invoke_id)
+        self.assertEqual(bytearray(header1.target_net_id.b),
+                         bytearray(header2.target_net_id.b))
+        self.assertEqual(header1.target_port, header2.target_port)
+        self.assertEqual(bytearray(header1.source_net_id),
+                         bytearray(header2.source_net_id))
+        self.assertEqual(header1.source_port, header2.source_port)
+        self.assertEqual(header1.command_id, header2.command_id)
+        self.assertEqual(header1.state_flags, header2.state_flags)
+        self.assertEqual(header1.data_length, header2.data_length)
+        self.assertEqual(header1.error_code, header2.error_code)
+        self.assertEqual(header1.invoke_id, header2.invoke_id)
+
+    def test_ams_packet(self):
+        source_addr = AmsAddr('127.0.0.1.1.1', 59558)
+        target_addr = AmsAddr('172.38.1.1.1.1', 59559)
+
+        header1 = structs.AmsHeader(
+            target_net_id=target_addr.netIdStruct(),
+            target_port=target_addr.port,
+            source_net_id=source_addr.netIdStruct(),
+            source_port=source_addr.port,
+            command_id=pyads.constants.ADSCOMMAND_READDEVICEINFO,
+            state_flags=pyads.constants.ADSSTATEFLAG_COMMAND,
+            data_length=6,
+            error_code=1,
+            invoke_id=2,
+        )
+        ads_data = b'\x00' * 6
+        amstcpheader = structs.AmsTcpHeader(header1.length + len(ads_data))
+        packet = structs.AmsPacket(amstcpheader, header1, ads_data)
+
+        # first six bytes should be amstcpheader
+        self.assertEqual(packet.to_bytes()[:6], amstcpheader.to_bytes())
+
+        # next 32 bytes should be ams header
+        self.assertEqual(packet.to_bytes()[6:38], header1.to_bytes())
+
+        # last bytes should be data
+        self.assertEqual(packet.to_bytes()[38:44], ads_data)
+
+        # check from bytes function
+        self.assertEqual(
+            structs.AmsPacket.from_bytes(packet.to_bytes()).to_bytes(),
+            packet.to_bytes()
+        )
 
     def test_ads_notification_sample(self):
         sample = structs.AdsNotificationSample(
