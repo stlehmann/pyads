bcee1e5d8a754e07e078fe3848cf754bae94521f
Stefan Lehmann <stlm@posteo.de>
2018-05-16 13:25:16 +0200

    Add tests for set_local_address

--- tests/test_ads.py
+++ tests/test_ads.py
@@ -1,3 +1,4 @@
+import pyads
 from pyads import AmsAddr
 import unittest
 
@@ -23,6 +24,34 @@ class AdsTest(unittest.TestCase):
         for i in range(len(ams_addr_numbers)):
             self.assertEqual(ams_addr_numbers[i], netid_numbers[i])
         self.assertEqual(adr.port, adr._ams_addr.port)
+
+    def test_set_local_address(self):
+        pyads.open_port()
+        org_adr = pyads.get_local_address()
+        org_netid = org_adr.netid
+
+        # Set netid to specific value
+        pyads.set_local_address('0.0.0.0.1.5')
+        netid = pyads.get_local_address().netid
+        self.assertEqual(netid, '0.0.0.0.1.5')
+
+        # Change netid by String
+        pyads.set_local_address('0.0.0.0.1.6')
+        netid = pyads.get_local_address().netid
+        self.assertEqual(netid, '0.0.0.0.1.6')
+
+        # Change netid by Struct
+        pyads.set_local_address(org_adr.netIdStruct())
+        netid = pyads.get_local_address().netid
+        self.assertEqual(netid, org_netid)
+
+        # Check inadequate netid
+        with self.assertRaises(ValueError):
+            pyads.set_local_address('1.2.3.a')
+
+        # Check wrong netid datatype
+        with self.assertRaises(AssertionError):
+            pyads.set_local_address(123)
 
 
 if __name__ == '__main__':
