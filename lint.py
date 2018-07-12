322a40eca0faee7e30d5ab1d8fec96f8d455df2d
Stefan Lehmann <stlm@posteo.de>
2018-07-12 13:45:28 +0200

    add linting script

---
+++ lint.py
@@ -0,0 +1,31 @@
+"""Script for linting the current project.
+
+:author: Stefan Lehmann <stlm@posteo.de>
+:license: MIT, see license file or https://opensource.org/licenses/MIT
+
+:created on 2018-06-28 15:10:47
+:last modified by:   Stefan Lehmann
+:last modified time: 2018-07-12 13:45:36
+
+"""
+import click
+import subprocess
+
+
+PACKAGE_NAME = 'pyads'
+
+
+click.echo('\n--- Running Mypy ---')
+res = subprocess.call(['mypy', PACKAGE_NAME])
+if res == 0:
+    click.echo(click.style('OK', fg='green'))
+
+click.echo('\n--- Running Flake8 ---')
+res = subprocess.call(['flake8'])
+if res == 0:
+    click.echo(click.style('OK', fg='green'))
+
+click.echo('\n--- Running pydocstyle ---')
+res = subprocess.call(['pydocstyle', PACKAGE_NAME])
+if res == 0:
+    click.echo(click.style('OK', fg='green'))
