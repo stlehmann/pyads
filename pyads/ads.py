cedb37f025bbe250902880a6716e04aa262261dd
Stefan Lehmann <stlm@posteo.de>
2018-09-10 10:47:08 +0200

    Update ads.py

--- pyads/ads.py
+++ pyads/ads.py
@@ -719,8 +719,8 @@ class Connection(object):
                 if plc_datatype == PLCTYPE_STRING:
                     dest = (c_ubyte * data_size)()
                     memmove(addressof(dest), addressof(data), data_size)
-                    # I had some NULL bytes in my string...
-                    value = bytearray(dest).split(b'\0',1)[0].decode('utf-8')
+                    # read only until null-termination
+                    value = bytearray(dest).split(b'\0', 1)[0].decode('utf-8')
 
                 elif plc_datatype not in datatype_map:
                     value = data
