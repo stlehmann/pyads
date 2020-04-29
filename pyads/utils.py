ac2809f4b0c4a6ec77b6f61b24a9060bb992dbb7
Stefan Lehmann <stlm@posteo.de>
2020-04-29 08:25:33 +0200

    Add deprecated decorator

--- pyads/utils.py
+++ pyads/utils.py
@@ -9,7 +9,9 @@
 :last modified time: 2018-07-12 14:11:12
 
 """
+import functools
 import sys
+import warnings
 from ctypes import c_ubyte
 
 
@@ -24,3 +26,21 @@ def platform_is_windows():
     """Return True if current platform is Windows."""
     # cli being .NET (IronPython)
     return sys.platform == "win32" or sys.platform == "cli"
+
+
+def deprecated(message):
+    """Decorator for deprecated functions.
+
+    Shows a deprecation warning with the given message if the
+    decorated function is called.
+
+    """
+    def decorator(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(message, DeprecationWarning)
+            return func(*args, **kwargs)
+
+        return wrapper
+    return decorator
