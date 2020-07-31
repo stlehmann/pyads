f8b12d6ccad7451f5556032b917d12c1e42e9f37
Stefan Lehmann <stlm@posteo.de>
2020-07-31 10:16:39 +0200

    Annotate utils.py

--- pyads/utils.py
+++ pyads/utils.py
@@ -13,21 +13,21 @@ import functools
 import sys
 import warnings
 
+from typing import Callable, Any, Optional
 
-def platform_is_linux():
-    # type: () -> bool
+
+def platform_is_linux() -> bool:
     """Return True if current platform is Linux or Mac OS."""
     return sys.platform.startswith("linux") or sys.platform.startswith("darwin")
 
 
-def platform_is_windows():
-    # type: () -> bool
+def platform_is_windows() -> bool:
     """Return True if current platform is Windows."""
     # cli being .NET (IronPython)
     return sys.platform == "win32" or sys.platform == "cli"
 
 
-def deprecated(message=None):
+def deprecated(message: Optional[str] = None) -> Callable:
     """Decorator for deprecated functions.
 
     Shows a deprecation warning with the given message if the
@@ -37,10 +37,10 @@ def deprecated(message=None):
     if message is None:
         message = "Deprecated. This function will not be available in future versions."
 
-    def decorator(func):
+    def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            warnings.warn(message, DeprecationWarning)
+        def wrapper(*args: Any, **kwargs: Any) -> Callable:
+            warnings.warn(message, DeprecationWarning)  # type: ignore
             return func(*args, **kwargs)
 
         return wrapper
