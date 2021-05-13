ef405f24db31f80f06963b0204d3c8b337a4913b
Stefan Lehmann <stlm@posteo.de>
2021-05-13 15:28:14 +0200

    Revert "Add original docstring"
    This reverts commit 1f2674b1

--- pyads/testserver/__init__.py
+++ pyads/testserver/__init__.py
@@ -1,19 +1,5 @@
-""" ADS TCP/IP testserver implementation.
+"""The testserver module of pyads."""
 
-ADS TCP/IP testserver implementation to allow for functional testing of
-the ADS protocol without connection to a physical device.
-Consists of a server thread which will listen for connections and delegate
-each new connection to a separate client thread, allowing for multiple clients
-to connect at once.
-Each client connection thread listens for incoming data, and delegates parsing
-and response construction to the handler. A handler function is injectable at
-server level by specifying the `handler` kwarg in the server constructor.
-
-:author: David Browne <davidabrowne@gmail.com>
-:license: MIT, see license file or https://opensource.org/licenses/MIT
-:created on: 2018-06-11 18:15:53
-
-"""
 from .testserver import AdsTestServer
 from .basic_handler import BasicHandler
 from .advanced_handler import AdvancedHandler, PLCVariable
