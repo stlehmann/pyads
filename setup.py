2eb618ce8e71c37fc32cdf3e9c5eeeebc1d4c406
Robert <robert.soor@gmail.com>
2021-06-01 08:32:58 +0200

    Add pyads.testserver to setup.py (#249)

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ setup(
     long_description_content_type='text/markdown',
     author="Stefan Lehmann",
     author_email="Stefan.St.Lehmann@gmail.com",
-    packages=["pyads"],
+    packages=["pyads", "pyads.testserver"],
     package_data={'pyads': ['adslib.so']},
     requires=[],
     install_requires=[],
