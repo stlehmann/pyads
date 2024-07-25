8d00fbd597e25968b00d8688ce89ef405f2e0155
RobertoRoos <robert.soor@gmail.com>
2024-07-25 13:18:57 +0200

    Started migration to pyproject.toml, working on Windows

--- setup.py
+++ setup.py
@@ -1,56 +1,22 @@
 #! /usr/bin/env python
 # -*-coding: utf-8 -*-
-import io
 import glob
 import os
-import re
 import sys
 import shutil
 import subprocess
 import functools
 import operator
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 from setuptools.command.install import install as _install
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
 from distutils.command.sdist import sdist as _sdist
 
 
-def read(*names, **kwargs):
-    try:
-        with io.open(
-            os.path.join(os.path.dirname(__file__), *names),
-            encoding=kwargs.get("encoding", "utf8")
-        ) as fp:
-            return fp.read()
-    except IOError:
-        return ''
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 def platform_is_linux():
     return sys.platform.startswith('linux') or \
         sys.platform.startswith('darwin')
-
-
-def get_files_rec(directory):
-    res = []
-    for (path, directory, filenames) in os.walk(directory):
-        files = [os.path.join(path, fn) for fn in filenames]
-        res.append((path, files))
-    return res
-
-
-data_files = get_files_rec('adslib')
 
 
 def create_binaries():
@@ -118,27 +84,7 @@ class install(_install):
         _install.run(self)
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ['--cov-report', 'html', '--cov-report', 'term',
-                            '--cov=pyads']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 cmdclass = {
-    'test': PyTest,
     'build': build,
     'clean': clean,
     'sdist': sdist,
@@ -146,37 +92,6 @@ cmdclass = {
 }
 
 
-long_description = read('README.md')
-
-
 setup(
-    name="pyads",
-    version=find_version('pyads', '__init__.py'),
-    description="Python wrapper for TwinCAT ADS library",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author="Stefan Lehmann",
-    author_email="Stefan.St.Lehmann@gmail.com",
-    packages=["pyads", "pyads.testserver"],
-    package_data={'pyads': ['adslib.so']},
-    requires=[],
-    install_requires=[],
-    provides=['pyads'],
-    url='https://github.com/MrLeeh/pyads',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: Microsoft :: Windows :: Windows 7',
-        'Operating System :: POSIX :: Linux',
-    ],
     cmdclass=cmdclass,
-    data_files=data_files,
-    tests_require=['pytest', 'pytest-cov'],
-    has_ext_modules=lambda: True,
 )
