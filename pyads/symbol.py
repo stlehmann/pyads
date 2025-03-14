17465b8ee256484a01eeafd6663e46d497cd44a7
msfur <shan.fur@gmx.de>
2025-03-14 11:56:17 +0100

    Fixed an issue with auto_update in connection with structs (#427)
    * Adaptation of add_device_notification() so that the length of NotificationAttrib is correctly set to _structure_size if a structure is present.
    
    * Adaptation of add_device_notification() so that the length of NotificationAttrib is correctly set to _structure_size if a structure is present.
    
    * Improved implementation of checking if a struct is present.
    
    * Improved query of whether a structure is present.
    
    * Add a test for the add_notification() function with structures
    
    * Update routing.rst (#429)
    
    Small Typo Fix (you plc -> your plc)
    
    * Adaptation of add_device_notification() so that the length of NotificationAttrib is correctly set to _structure_size if a structure is present.
    
    * Improved implementation of checking if a struct is present.
    
    * Improved query of whether a structure is present.
    
    * Add a test for the add_notification() function with structures
    
    ---------
    
    Co-authored-by: Rich-Hyena <168060947+Rich-Hyena@users.noreply.github.com>
    Co-authored-by: msfur <shan.fur@isw.uni-stuttgart.de>

--- pyads/symbol.py
+++ pyads/symbol.py
@@ -252,7 +252,10 @@ class AdsSymbol:
         """
 
         if attr is None:
-            attr = NotificationAttrib(length=sizeof(self.plc_type))
+            if self.is_structure:
+                attr = NotificationAttrib(length=self._structure_size)
+            else:
+                attr = NotificationAttrib(length=sizeof(self.plc_type))
 
         handles = self._plc.add_device_notification(
             (self.index_group, self.index_offset), attr, callback, user_handle
