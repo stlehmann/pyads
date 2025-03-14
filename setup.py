b4dade069a2f7c3ac0cc083eb67195c7736f5870
Robert <robert.soor@gmail.com>
2025-03-14 12:25:42 +0100

    Dropped Python 3.7 and lower, included up to Python 3.12 (#399)
    * Dropped Python 3.7 and lower, included up to Python 3.12 + Added Python requirement to setup.py
    
    * Updated checkout and setup-python Github workflows

--- setup.py
+++ setup.py
@@ -109,12 +109,36 @@ class CustomBDistWheel(bdist_wheel):
 
 # noinspection PyTypeChecker
 setup(
-    cmdclass={
-        "build_py": CustomBuildPy,
-        "install": CustomInstall,
-        "bdist_wheel": CustomBDistWheel,
-    },
+    name="pyads",
+    version=find_version('pyads', '__init__.py'),
+    description="Python wrapper for TwinCAT ADS library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Stefan Lehmann",
+    author_email="Stefan.St.Lehmann@gmail.com",
+    packages=["pyads", "pyads.testserver"],
+    package_data={'pyads': ['adslib.so']},
+    python_requires='>=3.8.0',
+    requires=[],
+    install_requires=[],
+    provides=['pyads'],
+    url='https://github.com/MrLeeh/pyads',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: POSIX :: Linux',
+    ],
+    cmdclass=cmdclass,
+    data_files=data_files,
+    tests_require=['pytest', 'pytest-cov'],
+    has_ext_modules=lambda: True,
 )
-# See `pyproject.toml` for all package information
-
-# Also see `MANIFEST.in`
